3b5cd307f9c4a126f7b93e66ba7434ae540dc3c5
Les Pounder <lespounder@gmail.com>
2017-08-24 14:49:22 +0100

    created try..except

--- card-check.py
+++ card-check.py
@@ -4,9 +4,12 @@ import SimpleMFRC522
 
 reader = SimpleMFRC522.SimpleMFRC522()
 
-print("Hold a tag near the reader")
+print("To check the ID hold the card / tag near to the reader")
+print("When done press CTRL + C to exit")
 
-while True:
-    id, text = reader.read()
-    print("This card's ID is",id)
-    #print(text)
+try:
+	while True:
+        id, text = reader.read()
+        print("This card's ID is",id)
+except KeyboardInetrrupt:
+	print("Exit")
