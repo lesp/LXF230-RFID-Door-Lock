32fe2c589142dce5f5e7746c8c81b23f51579f40
Les Pounder <lespounder@gmail.com>
2017-08-24 14:50:45 +0100

    fixed mixed indentation

--- card-check.py
+++ card-check.py
@@ -8,8 +8,8 @@ print("To check the ID hold the card / tag near to the reader")
 print("When done press CTRL + C to exit")
 
 try:
-	while True:
+    while True:
         id, text = reader.read()
         print("This card's ID is",id)
 except KeyboardInetrrupt:
-	print("Exit")
+    print("Exit")
