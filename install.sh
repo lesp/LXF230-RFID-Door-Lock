0774eace23c7dea48d8af4defe800007de89b321
Les Pounder <lespounder@gmail.com>
2017-08-24 14:42:40 +0100

    added install.sh to make it easy to install

---
+++ install.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+echo "Installation of the SPI library is thanks to Simon Monk's Clever Card Kit"
+echo "Take a look at https://github.com/simonmonk/clever_card_kit for more information"
+cd /home/$USER
+sudo apt-get install python3-dev
+git clone https://github.com/simonmonk/SPI-Py.git
+cd SPI-Py
+sudo python3 setup.py install
+cd /home/$USER
+git clone https://github.com/lesp/LXF230-RFID-Door-Lock
