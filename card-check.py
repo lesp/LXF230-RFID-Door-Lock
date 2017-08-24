a1f8b0e8de75f452ee53a585c2d232fccb2bf169
Les Pounder <lespounder@gmail.com>
2017-08-24 14:47:21 +0100

    updated card read as it produces errors

--- card-check.py
+++ card-check.py
@@ -7,6 +7,6 @@ reader = SimpleMFRC522.SimpleMFRC522()
 print("Hold a tag near the reader")
 
 while True:
-    id = reader.read()
+    id, text = reader.read()
     print("This card's ID is",id)
     #print(text)
